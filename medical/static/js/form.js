f316bdeb2518d0d9312a401366eb933b65ae77a2
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-22 23:16:10 +0300

    ft: enable toggle between register inputs js

--- medical/static/js/form.js
+++ medical/static/js/form.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
     $('#btn1').click(function(){
-        $('.p1').css('display', 'none')
-        $('.p2').css('display', 'block')
+        validatep1()
+    })
+    $('#btn2').click(function(){
+        $('.p1').css('display', 'block')
+        $('.p2').css('display', 'none')
     })
 })
+
+function validatep1(){
+    if ($('#first_name').val() == '' | $('#second_name').val() == '' | $('#email').val() == ''){
+        alert('Fill in all the inputs to proceed')
+    } else{
+        $('.p1').css('display', 'none')
+        $('.p2').css('display', 'block')
+    }
+}
