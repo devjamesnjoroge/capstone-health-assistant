8a245451679a132f64b8748a8ced8ec4630b9445
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-22 22:58:39 +0300

    ft: register template and views

---
+++ medical/static/js/form.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#btn1').click(function(){
+        $('.p1').css('display', 'none')
+        $('.p2').css('display', 'block')
+    })
+})
--- medical/views.py
+++ medical/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -6,3 +7,17 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'index.html')
+
+def register(request):
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+        user.save()
+        return redirect('/')
+
+    return render(request, 'authentication/register.html')
