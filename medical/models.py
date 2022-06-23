4a50eed4d58a881db64771e908d4e5abe8eabe2f
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-23 15:00:58 +0300

    ft: create profile model

--- medical/models.py
+++ medical/models.py
@@ -1,3 +1,21 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = CloudinaryField('images')
+    age = models.PositiveIntegerField()
+    bio = models.CharField(max_length=100)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __repr__(self):
+        print(self.user.username)
